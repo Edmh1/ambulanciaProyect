6bf0c31a09949d2919eb0e420a77f09a930d207b
Eddie <edavidmanotas@gmail.com>
2023-11-14 23:08:43 -0500

    main

---
+++ src/archivosVisuales/Ambulancia.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package archivosVisuales;
+
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author edavi
+ */
+public class Ambulancia {
+    
+    private String ambulancia = "ambulancia.gif";
+    
+    private int dx;
+    private int dy;
+    private int x;
+    private int y;
+    
+    private Image imagen;
+    
+    public Ambulancia(){
+        x = 40;
+        y = 60;
+        ImageIcon img = new ImageIcon(this.getClass().getResource(ambulancia));
+        imagen = img.getImage().getScaledInstance(20, 20, 0);
+    }
+    public void mover(){
+        x += dx;
+        y += dy;
+    }
+    public int tenerX(){
+        return x;
+    }
+    public int tenerY(){
+        return y;
+    }
+    public Image tenerImagen(){
+        return imagen;
+    }
+    
+    public void keyReleased(KeyEvent e){
+        int key = e.getKeyCode();
+
+        if(key == KeyEvent.VK_LEFT){
+            dx = 0;
+        }
+        if(key == KeyEvent.VK_RIGHT){
+            dx = 0;
+        }
+        if(key == KeyEvent.VK_UP){
+            dy = 0;
+        }
+        if(key == KeyEvent.VK_DOWN){
+            dy = 0;
+        }
+    }
+
+    public void keyPressed(KeyEvent e){
+        int key = e.getKeyCode();
+
+        if(key == KeyEvent.VK_LEFT){
+            dx = -5;
+        }
+        if(key == KeyEvent.VK_RIGHT){
+            dx = 5;
+        }
+        if(key == KeyEvent.VK_UP){
+            dy = -5;
+        }
+        if(key == KeyEvent.VK_DOWN){
+            dy = 5;
+        }
+    }
+
+    
+    
+    
+    
+}
---
+++ src/archivosVisuales/Dibujar.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package archivosVisuales;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ *
+ * @author edavi
+ */
+public class Dibujar extends JPanel implements ActionListener{
+
+    private Ambulancia ambulancia;
+    private Timer timer;
+    
+    public Dibujar() {
+        setBackground(Color.WHITE);
+        setFocusable(true);
+        addKeyListener((KeyListener) new Teclado());  
+        
+        ambulancia = new Ambulancia();
+        timer = new Timer(15, this);
+        timer.start();
+    }
+
+    
+    @Override
+    protected void paintComponent(Graphics grafica) {
+        super.paintComponent(grafica);
+        Graphics2D g2 = (Graphics2D) grafica;
+        g2.drawImage(ambulancia.tenerImagen(), ambulancia.tenerX(), ambulancia.tenerY(), null);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        ambulancia.mover();
+        repaint();
+    }
+    
+    private class Teclado extends KeyAdapter {
+        @Override
+        public void keyReleased(KeyEvent e) {
+            ambulancia.keyReleased(e);
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            ambulancia.keyPressed(e);
+        }
+    }
+
+    
+}
